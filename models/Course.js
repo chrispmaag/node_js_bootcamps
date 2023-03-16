3ad1214d108e0797fb20d52d834b7e5b33d3c809
Chris PMaag <chrispmaag@gmail.com>
2023-03-16 11:00:23 -0700

    feat: Add user auth for bootcamp & courses

--- models/Course.js
+++ models/Course.js
@@ -36,6 +36,11 @@ const CourseSchema = new mongoose.Schema({
     ref: 'Bootcamp',
     require: true,
   },
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    require: true,
+  },
 });
 
 // Static method to get avg of course tuitions
